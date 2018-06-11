4b9d33179d0d3e2b333c9eee163506d26014bea8
Alexander Belopolsky <a@enlnt.com>
2018-06-11 19:46:42 -0400

    Add 64-bit environment

---
+++ .travis/getkdb.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "${CONDA_FORCE_32BIT}" = "1" ]; then
+  curl -O https://kx.com/$X/3.5/linuxx86.zip
+  unzip -d $CONDA_PREFIX linuxx86.zip
+  rm -f $CONDA_PREFIX/q/q.q
+else
+  curl -O -L $L64
+  mkdir -p $CONDA_PREFIX/q
+  unzip -d $CONDA_PREFIX/q l64.zip
+fi
