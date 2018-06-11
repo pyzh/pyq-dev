5f9fe724b9e356d3b09863744a8fb4c2070f8548
Alexander Belopolsky <a@enlnt.com>
2018-06-11 17:29:27 -0400

    More C coverage

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,13 +7,14 @@ if [ -n "${CONDA_FORCE_32BIT}" ]; then
 else
    arch=""
 fi
-python setup.py install_exe
 python setup.py install_qlib
-python setup.py install_qext
 python setup.py install_scripts
 LDFLAGS="${arch}--coverage -lgcov"
 CFLAGS="${arch}--coverage"
-export CFLAGS LDFLAGS
+CC="gcc $LDFLAGS"
+export CFLAGS LDFLAGS CC
 python setup.py build_ext --build-temp .
 python setup.py install_lib
-unset CFLAGS LDFLAGS
+python setup.py install_exe
+python setup.py install_qext
+unset CFLAGS LDFLAGS CC
