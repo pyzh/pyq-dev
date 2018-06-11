fc7e16de87b630ab2d8d86246addce7b542369df
Alexander Belopolsky <a@enlnt.com>
2018-06-11 16:35:27 -0400

    Separate travis build commands in a separate file.

---
+++ .travis/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ -n "${CONDA_FORCE_32BIT}"]; then
+   arch=-m32
+else
+   arch=""
+fi
+python setup.py install_exe
+python setup.py install_qlib
+python setup.py install_qext
+python setup.py install_scripts
+LDFLAGS='$arch --coverage -lgcov'
+CFLAGS='$arch --coverage'
+export CFLAGS LDFLAGS
+python setup.py build_ext --build-tmp .
+python setup.py install_lib
+unset CFLAGS LDFLAGS
